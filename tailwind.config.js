c9c289350b3e68a5f907715fc48a321ce146dab4
Rebeca <rcks.santos@gmail.com>
2021-09-11 16:33:17 -0300

    :lipstick: Customiza tema do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
